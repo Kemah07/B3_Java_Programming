4227e0194e4b6979777061e6ac11b227564bf14d
kananmahmudov <kenan_mekanli@yahoo.com>
2024-05-30 22:28:32 -0400

    constructor chaining

--- src/day31_custom_classes/Phone.java
+++ src/day31_custom_classes/Phone.java
@@ -24,13 +24,33 @@ public class Phone {
         this.name = name; // phone1.name = name;
         this.brand = brand;
     }
+
     @Override
     public String toString() {
-        return "Phone{" +
-                "name='" + name + '\'' +
-                ", brand='" + brand + '\'' +
-                ", memory=" + memory +
-                ", version=" + version +
-                '}';
+
+        String str = name;
+
+        if (brand != null) {  // if it is not default value
+            str += " - " + brand;
+        }
+
+        if (memory != 0) {
+            str += " - " + memory;
+        }
+
+        if (version != 0.0){
+            str += " - " + version;
+        }
+
+//        return "Phone{" +
+//                "name='" + name + '\'' +
+//                ", brand='" + brand + '\'' +
+//                ", memory=" + memory +
+//                ", version=" + version +
+//                '}';
+
+
+        return str;
     }
 }
+
