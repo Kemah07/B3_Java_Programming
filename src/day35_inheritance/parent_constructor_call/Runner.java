9d051159a399de34bed356651ebcd081b40a99d5
kananmahmudov <kenan_mekanli@yahoo.com>
2024-06-08 01:54:35 -0500

    super(). super., inheritance

---
+++ src/day35_inheritance/parent_constructor_call/Runner.java
@@ -0,0 +1,9 @@
+package day35_inheritance.parent_constructor_call;
+
+public class Runner {
+    public static void main(String[] args) {
+        //B bObj1 = new B();
+        //A aObj1 = new A();
+
+    }
+}
